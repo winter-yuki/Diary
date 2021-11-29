2bbe20073f414af84e62a257459310dff2249bce
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-11-29 18:06:45 +0300

    Add links todos

--- src/main/kotlin/Pdf.kt
+++ src/main/kotlin/Pdf.kt
@@ -4,18 +4,20 @@ interface Link {
     fun jump()
 }
 
-class PdfLink private constructor(private val path: Path) : Link {
+class PdfLink private constructor(
+    private val path: Path, private val anchor: Nothing
+) : Link {
     override fun jump() {
-        TODO("Jump to PDF is not implemented")
+        TODO("Jump to PDF is not implemented") // #17
     }
 
     companion object {
-        fun of(path: Path) = PdfLink(path)
+        fun of(path: Path) = PdfLink(path, TODO())
     }
 }
 
 class CellLink : Link {
     override fun jump() {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented") // #18
     }
 }
