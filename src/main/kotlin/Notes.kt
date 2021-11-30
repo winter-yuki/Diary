12f14b1f642f21c006e4ce3c58636157fe326498
Grigory Raykin <gregra@mail.ru>
2021-11-30 18:43:11 +0300

    Colored close cell button

--- src/main/kotlin/Notes.kt
+++ src/main/kotlin/Notes.kt
@@ -6,19 +6,18 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.intellij.lang.annotations.JdkConstants
-import org.jetbrains.skija.paragraph.TextBox
 import java.nio.file.Path
+
 
 class Notes(private val cells: SnapshotStateList<Cell>) {
 
@@ -59,12 +58,25 @@ class Notes(private val cells: SnapshotStateList<Cell>) {
             modifier = Modifier.fillMaxWidth().fillMaxHeight()
         ) {
             Column(modifier = Modifier.padding(vertical = 4.dp).fillMaxWidth(0.75F)) {
-                Button(
-                    modifier = Modifier
-                        .size(width = 30.dp, height = 25.dp)
-                        .align(Alignment.End),
-                    onClick = { cells.removeAt(iCell) }
-                ) {}
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(1.dp),
+                    verticalArrangement = Arrangement.spacedBy(24.dp)
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .size(width = 40.dp, height = 40.dp)
+                            .align(Alignment.End)
+                            .padding(1.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Red,
+                            contentColor = Color.Black),
+                        onClick = { cells.removeAt(iCell) }
+                    ) {
+                        Text("X", fontSize = 10.sp)
+                    }
+                }
                 block()
                 Row(
                 ) {
