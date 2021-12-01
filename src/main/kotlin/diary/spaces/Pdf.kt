d5cb2b6cd0ccf4d7cd99e098dba191a901b95977
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-12-01 19:09:10 +0300

    Pdf viewer

--- src/main/kotlin/diary/spaces/Pdf.kt
+++ src/main/kotlin/diary/spaces/Pdf.kt
@@ -1,24 +1,72 @@
 package diary.spaces
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeBitmap
+import androidx.compose.ui.unit.dp
 import diary.ui.UIElem
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.rendering.PDFRenderer
 import java.nio.file.Path
 
-class Pdf : UIElem {
+class Pdf(doc: PDDocument) : UIElem {
+
+    private val renderer: PDFRenderer = PDFRenderer(doc)
+    private var currPage: Int = 0
+    private val nPages: Int = doc.numberOfPages
+
     @Composable
     override fun invoke() {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text("I'm PDF!", modifier = Modifier.align(Alignment.Center))
+        Box(modifier = Modifier.fillMaxSize().padding(10.dp)) {
+            var image by remember { mutableStateOf(render()) }
+            Column(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    image,
+                    contentDescription = "Page $currPage",
+                    modifier = Modifier
+                        .fillMaxHeight(0.95F)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                    TextButton(
+                        onClick = {
+                            if (currPage > 0) {
+                                currPage--
+                                image = render()
+                            }
+                        },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text("Prev")
+                    }
+                    TextButton(
+                        onClick = {
+                            if (currPage + 1 < nPages) {
+                                currPage++
+                                image = render()
+                            }
+                        },
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Text("Next")
+                    }
+                }
+            }
         }
         // TODO
     }
 
+    // TODO remove borders
+    private fun render(): ImageBitmap =
+        renderer.renderImage(currPage).toComposeBitmap()
+
     companion object {
-        fun from(path: Path): Pdf = Pdf() // TODO
+        fun from(path: Path) = Pdf(PDDocument.load(path.toFile()))
     }
 }
