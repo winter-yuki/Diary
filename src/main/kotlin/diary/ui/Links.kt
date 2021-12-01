00565851f2c48ce2a514f17d7763ec8db786a143
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-12-01 23:55:46 +0300

    Remove comments

--- src/main/kotlin/diary/ui/Links.kt
+++ src/main/kotlin/diary/ui/Links.kt
@@ -5,23 +5,3 @@ interface Link {
 }
 
 class LinkBuffer(var link: Link? = null)
-
-
-//
-//class PdfLink private constructor(
-//    private val path: Path, private val anchor: Nothing
-//) : Link {
-//    override fun jump() {
-//        TODO("Jump to PDF is not implemented") // #17
-//    }
-//
-//    companion object {
-//        fun of(path: Path) = PdfLink(path, TODO())
-//    }
-//}
-//
-//class CellLink : Link {
-//    override fun jump() {
-//        TODO("Not yet implemented") // #18
-//    }
-//}
