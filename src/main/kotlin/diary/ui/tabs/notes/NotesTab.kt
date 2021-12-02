45ee6445d9d902d6594bf6dbca3f9560eca10a44
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2021-12-02 19:51:39 +0300

    Make compile

--- src/main/kotlin/diary/ui/tabs/notes/NotesTab.kt
+++ src/main/kotlin/diary/ui/tabs/notes/NotesTab.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.loadImageBitmap
